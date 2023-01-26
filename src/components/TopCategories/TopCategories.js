e494567eb97301c892cd2c3fffa2255eba5e9892
filippopendyk <filipos28511@gmail.com>
2023-01-26 00:09:35 -0500

    applies style on active element in top cat nav

--- src/components/TopCategories/TopCategories.js
+++ src/components/TopCategories/TopCategories.js
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom"
 import topCategoriesSlice from "./topCategoriesSlice"
+import './topCategories.css';
 
 export default function TopCategories(){
     return (
-    <div className="p-8 flex flex-col">
+    <nav className="p-8 flex flex-col">
         {topCategoriesSlice.map((element) => {
             return <NavLink to={"/" + element.name}>{element.name}</NavLink>;
         })}
-    </div>
+    </nav>
     );
 }
