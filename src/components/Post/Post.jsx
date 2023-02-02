5f90eae1091df0b653c8091d03a7b1062bba2827
filippopendyk <filipos28511@gmail.com>
2023-02-02 18:02:00 -0500

    styles the displayed post component

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,16 +1,34 @@
 import { FaAngleUp } from 'react-icons/fa';
 import { FaAngleDown } from 'react-icons/fa'
+import { IconContext } from 'react-icons';
+import Numeral from 'react-numeral';
+import "./Post.css";
+
 
 export default function Post({post}){
 
+    console.log(post);
+
     return (
-        <div className="flex bg-post-ups-bg">
-            <div className="flex flex-col">
-                <FaAngleUp/>
-                    {post.ups}
-                <FaAngleDown/>
+        <div className="flex mb-6 post-container">
+            <div className="flex flex-col bg-post-ups-bg p-4 text-center items-center w-12">
+                <IconContext.Provider value={{margin: "auto", textAlign: "center"}}>
+                    <FaAngleUp/>
+                    {
+                        post.ups > 1000 ? <Numeral value={(post.ups)}  format={"0.0a"}/> : post.ups
+                    }
+                    <FaAngleDown/>
+                </IconContext.Provider>
             </div>
-            <div>
+            <div className='px-2 py-1 bg-[#fff] post-content-container'>
+                <p>
+                <strong>{post.subreddit_name_prefixed} </strong>
+                Posted by {post.author}
+                </p>
+                <h2>{post.title ? post.title : post.subtitle}</h2>  
+                {
+                    post.thumbnail.includes("https://") ? <img src={post.thumbnail} alt="post thumbnail" sizes="250" width="250" className='py-2'/> : null
+                }
 
             </div>
         </div>
